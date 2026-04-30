908a710c0b7d91f8b3da382278313d35804a2e9a
Mukhtar <mukhtarrodinal633@gmail.com>
2026-04-30 11:00:17 +0700

    Fungsi Kalku

--- Resouce C/Kalkulator.c
+++ Resouce C/Kalkulator.c
@@ -10,9 +10,9 @@ float mul(float a, float b) {
     return a * b;
 }
 float div(float a, float b) {
-    if (b == 0) {
-        printf("Error: Tidak bisa dibagi dengan nol!\n");
-        return 0;
-    }
-    return a / b;
+if (b == 0) {
+    printf("Error: Tidak bisa dibagi dengan nol!\n");
+    return 0;
+}
+return a / b;
 }
