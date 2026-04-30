9026935f362b1ff343770d614d6ab9eae5e19eca
Mukhtar <mukhtarrodinal633@gmail.com>
2026-04-30 10:56:19 +0700

    Update Kalkulator.c

--- Resouce C/Kalkulator.c
+++ Resouce C/Kalkulator.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
-#include <string.h>
+
+loat add(float a, float b) {
+    return a + b;
+}
+float sub(float a, float b) {
+    return a - b;
+}
+float mul(float a, float b) {
+    return a * b;
+}
+float div(float a, float b) {
+    if (b == 0) {
+        printf("Error: Tidak bisa dibagi dengan nol!\n");
+        return 0;
+    }
+    return a / b;
+}
